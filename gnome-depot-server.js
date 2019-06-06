e9a2923d7da7855277df0cfd60c18a3b83b4c78c
Azeban <richarsc@oregonstate.edu>
2019-06-05 18:37:24 -0700

    Base mostly finished & templatized homepage

--- gnome-depot-server.js
+++ gnome-depot-server.js
@@ -21,6 +21,7 @@ var http = require('http');
 var fs = require('fs');
 var path = require('path');
 var express = require('express');
+var exphbs = require('express-handlebars');
 
 /*
 var index_html = fs.readFileSync('./public/index.html');
@@ -34,14 +35,25 @@ var html_404 = fs.readFileSync('./public/404.html');
 var app = express();
 var port = process.env.PORT || 6009;
 
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
+
 app.use(express.static('public'));
 
 app.get('/', function (req, res, next) {
 
-	res.status(200).render('homePage');
+	res.status(200).render('partials/home');
   
-  });
+});
 
+// app.get('/:page', function(req, res, next) {
+// 	// if pages exists
+// 	// then serve that page,
+// 	// else
+// 	// next();
+// })
 // app.get('*', function (req, res) {
 // 	res.status(404).sendFile(path.join(__dirname, 'public', '404.html'));
 // });
